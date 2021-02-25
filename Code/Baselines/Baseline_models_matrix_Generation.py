22332903a40b90a045ad6877f9bef4ba9f2e6aa6
Sarwan Pasha <sarwanpasha@gmail.com>
2021-02-25 06:38:16 +0500

    Add files via upload

--- Code/Baselines/Baseline_models_matrix_Generation.py
+++ Code/Baselines/Baseline_models_matrix_Generation.py
@@ -19,29 +19,51 @@ Created on Mon Feb 22 21:50:56 2021
 import matplotlib.pyplot as plt
 import pydicom
 from pydicom.data import get_testdata_files
-import struct
 import numpy as np
-from skimage.transform import resize
+import struct
 from os import listdir
 from os.path import isfile, join
 import csv
+import pandas as pd
+
+from skimage.transform import resize
+
+
+from numpy import mean
+from numpy import std
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import TruncatedSVD
+from sklearn.linear_model import LogisticRegression
+
+
 
 #get filenames of all dicom images in the dataset folder
-dataset_path = './dataset'
+#dataset_path = 'D:/University/Ph.D/Advanced Machine Learning/Project/Code/dataset'
+dataset_path = 'G:/Train Data/'
 onlyfiles = [f for f in listdir(dataset_path) if isfile(join(dataset_path, f))]
+onlyfiles = onlyfiles[1:1000]
 
-    
+
+train_data = pd.read_csv (r'D:/University/Ph.D/Advanced Machine Learning/Project/Code/train.csv')
+# 0 index has image ID
+# 2 index has class id
+
+
 #we select minimum height and width for the images in the whole dataset
 #this is done to make all images of same size
 #so that we can make equal length feature vectors
 min_width = 823	
 min_height = 927
 
+temp_length = min_width*min_height+1
 temp_matrix = np.zeros((len(onlyfiles),min_width*min_height))
 
 for i in range(len(onlyfiles)):
 #    print("i = ",i)
-    image_path = './' + onlyfiles[i]
+    image_path = dataset_path + onlyfiles[i]
     
     dataset= pydicom.read_file(image_path)
     pixel_values = dataset.pixel_array
@@ -61,5 +83,9 @@ for i in range(len(onlyfiles)):
     print("Average = ",np.mean(resized_vector))
     temp_matrix[i,] = resized_vector
     print("Average mat row = ",np.mean(temp_matrix[i,]))
+    
 
+# Write to the pdf
+df = pd.DataFrame(temp_matrix)
 
+df.to_csv (r'D:/University/Ph.D/Advanced Machine Learning/Project/Code/Preprocessed_Original.csv', index = False, header=True)
